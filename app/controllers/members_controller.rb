3afde115920e25fb9742918e6b15336d9d72fd6a
nagasho1122 <nagasetopuun1122@icloud.com>
2021-09-20 02:14:04 +0000

    Finish members index setting

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,7 +2,22 @@ class MembersController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
   
   def index
-    @members = Member.all
+    year = DateTime.now.year
+    if DateTime.now.month <= 3
+      year -= 1
+    end
+    one_grade_duration_start = DateTime.new(year, 4, 1, 0, 0, 0)
+    one_grade_duration_end = DateTime.new(year+1, 3, 31,23, 59, 59)
+    second_grade_duration_start = DateTime.new(year-1, 4, 1, 0, 0, 0)
+    second_grade_duration_end = DateTime.new(year, 3, 31,23, 59, 59)
+    third_grade_duration_start = DateTime.new(year-2, 4, 1, 0, 0, 0)
+    third_grade_duration_end = DateTime.new(year-1, 3, 31,23, 59, 59)
+    fourth_grade_duration_start = DateTime.new(year-3, 4, 1, 0, 0, 0)
+    fourth_grade_duration_end = DateTime.new(year-2, 3, 31,23, 59, 59)
+    @members_one = Member.where(addmission_date:  one_grade_duration_start..one_grade_duration_end)
+    @members_second = Member.where(addmission_date:  second_grade_duration_start..second_grade_duration_end)
+    @members_third = Member.where(addmission_date:  third_grade_duration_start..third_grade_duration_end)
+    @members_fourth = Member.where(addmission_date:  fourth_grade_duration_start..fourth_grade_duration_end)
   end
   
   def new
---
+++ test/integration/members_index_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class MembersIndexTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @member1 = members(:example_member)
+    @member2 = members(:example_member_destroy)
+    @member3 = members(:example_member_thirdgrade)
+    @member4 = members(:example_member_forthgrade)
+  end
+  
+  test "should be valid grade layout" do
+    log_in_as
+    get members_path
+    assert_template "members/index"
+    assert_equal Member.count, 4
+    assert_equal @member1.addmission_date, DateTime.new(2021, 4, 1, 0, 0, 0)
+    assert_select "li.member", count: 4
+    delete member_path(@member2)
+    assert_redirected_to members_url
+    follow_redirect!
+    assert_select "li.member", count: 3
+  end
+  
+end
